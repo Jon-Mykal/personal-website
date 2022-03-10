f2d9041bac09521f2aba28cf977fed36c8862c3e
Gregg Wong <griekup@live.com>
2022-03-09 20:11:39 -0500

    Add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <nav>
+            <router-link to="/">Home</router-link> 
+            <router-link :to="{name: 'services'}">Services
+                <ul>
+                    <li>
+                        <router-link :to="{name: 'development'}">Development</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{name: 'design'}">Design</router-link>
+                    </li>
+                </ul>
+            </router-link>
+            <router-link to="/about">About</router-link> 
+            <router-link :to="{ name: 'contact'}">Get in touch</router-link>
+        </nav>
+    </section>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
