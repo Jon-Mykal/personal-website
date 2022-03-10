18b5691e0b7ff359edb88b96448cdb34090e20ad
Gregg Wong <griekup@live.com>
2022-03-10 17:38:14 -0500

    Create Footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer>
+        <p>
+            &copy; <span>{{ copyrightYear}}</span> Gregg Wong
+        </p>
+        <p>Made with <a href="https://vuejs.org">Vue.js</a></p>
+    </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    setup () {
+        const copyrightYear = computed(() => {
+            let date = new Date();
+            return date.getFullYear();
+        })
+
+        return {copyrightYear};
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
