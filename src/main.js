21b3d068e3602eb7e096a211348e9ab9db3f7bf6
Gregg Wong <griekup@live.com>
2022-03-10 17:38:40 -0500

    Import styling libs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import Hero from "@/components/Hero.vue"
 
+// App Styling
+// import 'primeflex/primeflex.css'
+// import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const app = createApp(App);
+// Global Component Registrations
 app.component('Hero', Hero);
 app.use(router).mount('#app')
