37043c50cc8ed1110df063c214e471f548f85591
Gregg Wong <griekup@live.com>
2022-03-09 20:13:17 -0500

    Add Navbar component for structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link :to="{ name: 'contact'}">Contact</router-link>
-  </nav>
+  <Navbar />
   <router-view/>
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue"
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
 
 <style>
 #app {
