a6763197085caab5df2d1e8eb8dad550f95d6cf6
Gregg Wong <griekup@live.com>
2022-03-10 17:38:57 -0500

    Style Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,77 @@
 <template>
     <section>
-        <nav>
-            <router-link to="/">Home</router-link> 
-            <router-link :to="{name: 'services'}">Services
-                <ul>
-                    <li>
-                        <router-link :to="{name: 'development'}">Development</router-link>
-                    </li>
-                    <li>
-                        <router-link :to="{name: 'design'}">Design</router-link>
-                    </li>
-                </ul>
-            </router-link>
-            <router-link to="/about">About</router-link> 
-            <router-link :to="{ name: 'contact'}">Get in touch</router-link>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <section class="container">
+                <router-link to="/" class="navbar-brand">Gregg Wong</router-link>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <section class="offcanvas offcanvas-end w-75" id="navbarSupportedContent" ref="offcanvas">
+                    <header class="offcanvas-header">
+                        <h5 class="offcanvas-title">Menu</h5>
+                        <button ref="closeBtn" type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </header>
+                    <section class="offcanvas-body justify-content-end">
+                        <ul class="navbar-nav">
+                            <li class="nav-tem">
+                                <router-link to="/" class="nav-link">Home</router-link> 
+                            </li>
+                            <li class="nav-tem">
+                                <router-link :to="{name: 'development'}" class="nav-link">Development</router-link>
+                            </li>
+                            <li class="nav-tem">
+                                <router-link :to="{name: 'design'}" class="nav-link">Design</router-link>
+                            </li>
+                            <li class="nav-tem">
+                                <router-link to="/about" class="nav-link">About</router-link> 
+                            </li>
+                            <li class="nav-tem ml-3">
+                                <router-link :to="{ name: 'contact'}" class="btn btn-primary rounded-pill nav-link px-3">Get in touch</router-link>
+                            </li>
+                        </ul>
+                    </section>
+                </section>
+            </section>
         </nav>
     </section>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
 export default {
     setup () {
-        
+        const router = useRouter();
+        const offcanvas = ref(null);
+        const closeBtn = ref(null);
 
-        return {}
+        router.afterEach(() => {
+            const menuOpened = offcanvas.value.classList.contains("show");
+           if (menuOpened) {
+                closeBtn.value.click();
+           }
+        });
+
+        return {offcanvas, closeBtn};
     }
 }
 </script>
 
 <style scoped>
+.navbar-light .navbar-toggler {
+    border-color: transparent;
+}
 
+.navbar-toggler {
+    box-shadow: none;
+}
 </style>
