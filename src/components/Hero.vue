db6afb8fa9d3221189ff7ddfb3382cc58766f27d
Gregg Wong <griekup@live.com>
2022-03-09 20:12:54 -0500

    Add Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+        <h1>{{pageTitle}}</h1>
+
+</template>
+
+<script>
+export default {
+    props: {
+        pageTitle: {
+            type: String,
+            required: true
+        }
+    },
+    setup (props) {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
