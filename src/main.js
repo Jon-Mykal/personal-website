db45603beccf1c3562924aeba7eb85ac0e582777
Gregg Wong <griekup@live.com>
2022-03-15 16:00:05 -0500

    Add PrimeVue dialog

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import Hero from "@/components/Hero.vue"
 
 // App Styling
 // import 'primeflex/primeflex.css'
-// import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import PrimeVue from 'primevue/config'
+import Dialog from 'primevue/dialog'
+
 const app = createApp(App);
 // Global Component Registrations
 app.component('Hero', Hero);
-app.use(router).mount('#app')
+app.component('Dialog', Dialog);
+app.use(router).use(PrimeVue).mount('#app');
