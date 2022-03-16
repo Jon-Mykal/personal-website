3161230a9e6dea3de3fd323ab9ef361ec769f0b9
Gregg Wong <griekup@live.com>
2022-03-16 13:02:57 -0500

    Add toasts/toast service

--- src/main.js
+++ src/main.js
@@ -12,10 +12,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import PrimeVue from 'primevue/config'
-import Dialog from 'primevue/dialog'
+import ToastService from 'primevue/toastservice'
+
+import Dialog from 'primevue/dialog';
+import Toast from 'primevue/toast';
+import BlockUI from 'primevue/blockui';
+import ProgressSpinner from 'primevue/progressspinner';
 
 const app = createApp(App);
 // Global Component Registrations
 app.component('Hero', Hero);
 app.component('Dialog', Dialog);
-app.use(router).use(PrimeVue).mount('#app');
+app.component('BlockUI', BlockUI);
+app.component('ProgressSpinner', ProgressSpinner);
+app.component('Toast', Toast);
+app.use(router).use(PrimeVue).use(ToastService).mount('#app');
