6696bc2969ae86d5b7bbebd09d9dd751f29b9f69
Gregg Wong <griekup@live.com>
2022-03-16 13:03:20 -0500

    Add styling placeholder

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <Hero pageTitle="About Me"/>
+    <section class="bio"></section>
+    <section class="learning"></section>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.bio {
+  background: #dedede;
+  height: 25rem;
+}
+
+.learning {
+  background: #cdcdcd;
+  height: 25rem;
+}
+</style>
