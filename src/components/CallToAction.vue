cf2e9ea7cc62892f82ba2c97b0ce3df12763b32a
Gregg Wong <griekup@live.com>
2022-03-17 22:09:09 -0500

    Create CallToAction component

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="bg-secondary pb-5 pt-4">
+         <section class="py-1"></section>
+        <section class="cta-main d-flex justify-content-center align-items-center flex-column">
+            <h3 class="pb-2">{{headerTitle}}</h3>  
+            <router-link :to="{ name: 'contact' }" class="btn btn-primary mt-3 rounded-pill py-3 px-5">GET IN TOUCH</router-link>
+            <section class="pb-5"></section>
+        </section>
+  
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        headerTitle: {
+            type: String,
+            required: true
+        }
+    },
+    setup (props) {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+.cta-main {
+    /* heigkht: 15rem; */
+}
+
+.wrapper {
+    /* max-height: 100%; */
+    height: 1rem;
+}
+
+</style>
