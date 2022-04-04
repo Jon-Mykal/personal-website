fae1c785b2c6da6707bb4b07d6a52db9c7abcd8c
Gregg Wong <gwong@jngijamaica.com>
2022-04-04 12:01:32 -0500

    Add base class colours

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,36 @@ import Footer from "@/components/Footer.vue"
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  --main-blue: #0166FE;
+  --dark-blue-black: #00112A;
+  --off-white-blue: #F9FCFF;
+  --soft-blue: #9EC5FF;
+  --deep-blue: #0048B3;
+  --yellow-orange: #FEAA01;
+}
+
+.gw--bg-main-blue {
+  background-color: var(--main-blue) !important;
+}
+
+.gw--text-main-blue {
+  color: var(--main-blue) !important;
+}
+
+.gw--bg-dark-blue-black {
+  background-color: var(--dark-blue-black) !important;
+}
+
+.gw--bg-yellow-orange {
+  background-color: var(--yellow-orange) !important;
+}
+
+.gw--bg-off-white-blue {
+  background-color: var(--off-white-blue) !important;
+}
+
+.gw--bg-off-white-blue-pattern {
+  background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='120' height='120' patternTransform='scale(.05) rotate(25)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(210, 100%, 99%, 1)'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='hsla(216, 100%, 81%, 1)'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")
 }
 
 nav {
@@ -57,4 +87,6 @@ li.p-carousel-indicator button.p-link {
 .tools-used {
     margin-bottom: -3.5rem;
 }
+
+
 </style>
