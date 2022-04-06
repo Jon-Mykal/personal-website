d10a8393d8ded13d1f0046f99005e4fe24039a4c
Gregg Wong <gwong@jngijamaica.com>
2022-04-05 21:24:56 -0500

    Add full bio, styling edits

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,23 +2,27 @@
   <section class="about">
     <Hero pageTitle="About Me"/>
     <section class="bio py-3">
-      <div class="py-4"></div>
+      <h3 class="py-3">Bio</h3>
       <section class="container">
         <section class="row pb-2">
-          <section class="col-md-6 col-sm-12">
+          <section class="col-md-6 col-sm-12 py-5">
             <section class="wrapper d-flex px-3 flex-column align-items-center pb-3">
-              <section class="img-frame"></section>
+              <section class="img-frame gw--shadow-yellow-orange"></section>
             </section>
           </section>
-          <section class="col-md-6 col-sm-12">
-            <section class="wrapper d-flex px-3 flex-column pb-3 text-md-start">
-              <p class="fs-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
-                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. 
-                A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              <p class="pt-1">
-                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
-                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. 
-                A small river named Duden flows by their place and supplies it with the necessary regelialia.
+          <section class="col-md-6 col-sm-12 py-5">
+            <section class="wrapper d-flex px-3 flex-column pb-2 text-md-start">
+              <p class="fs-5 pb-0 align-self-center align-self-md-start gw--p-adjustment">I'm Gregg Wong, a web/software developer &amp; designer based in the beautiful island of Jamaica currently stationed in St. Catherine.</p>
+              <p class="pb-0 align-self-center align-self-md-start gw--p-adjustment">
+                  I've spent the last <strong>7+ years</strong> working on development projects from building out UIs to upgrading existing apps by adding features to make users happy. 
+                  I've touched on a few application types such as desktop and web (Single Page Apps, traditional, APIs) and more recently added designer to my list of skills and talents.
+              </p>
+              <p class="pb-0 align-self-center align-self-md-start gw--p-adjustment">
+                  Being a self-described lifelong learner, I am always pushing myself to learn new things even outside of my chosen field as I believe awareness is always a good thing.
+              </p>
+              <p class="pb-0 align-self-center align-self-md-start gw--p-adjustment">
+                  Outside of work, I do enjoy playing football (no, not a "balla"), going to the occasional party and taking road trips. 
+                  While I can't show you here, music is also a thing of mine as I write and sing along with playing the piano.
               </p>
             </section>
             <section class="d-flex px-3 justify-content-center justify-content-md-start justify-content-sm-center">
@@ -28,11 +32,11 @@
         </section>
       </section>
     </section>
-    <section class="learning py-3">
+    <section class="learning py-3 gw--bg-off-white-blue-pattern">
       <h3 class="py-3">Currently Learning</h3>
       <section class="container">
         <section class="row pb-2 justify-content-center">
-          <section class="col-12 col-md-8 col-lg-6">
+          <section class="col-12 col-md-8 col-lg-6 pt-4 pb-2">
           <Carousel :value="currentlyLearning" :numVisible="1" :circular="true">
             <template #item="slotProps">
               <section class="pb-5">
@@ -91,19 +95,20 @@ export default {
 </script>
 
 <style scoped>
-.bio {
+/* .bio {
   background: #dedede;
 }
 
 .learning {
   background: #cdcdcd;
 
-}
+} */
 
 .img-frame {
-  height: 30rem;
-  width: 25rem;
+  height: 27rem;
+  width: 27rem;
   background: white;
+  border: 1px solid #dedede;
 }
 .circle {
   height: 10rem;
