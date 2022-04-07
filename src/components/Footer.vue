8fa73e6d6e84149d0ed2126585226a08e057205b
Gregg Wong <gwong@jngijamaica.com>
2022-04-07 16:40:13 -0500

    Mobile layout changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
     <footer class="gw--bg-dark-blue-black">
-        <section class="py-3 container-fluid d-flex justify-content-around align-items-center">
+        <section class="py-3 container-fluid d-flex justify-content-around align-items-center flex-column flex-sm-row">
             <p class="p-no-margin">
                 &copy; <span>{{ copyrightYear}}</span> Gregg Wong
             </p>
-            <p class="p-no-margin">Made with <a href="https://vuejs.org">Vue.js</a>. Powered by <a href="https://netlify.com">Netlify</a>.</p>
+            <p class="p-no-margin">Made with <a class="gw--text-main-blue text-decoration-none" href="https://vuejs.org">Vue.js</a>. Powered by <a class="gw--text-main-blue text-decoration-none" href="https://netlify.com">Netlify</a>.</p>
         </section>
     </footer>
 </template>
